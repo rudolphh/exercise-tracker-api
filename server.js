56e065d77f261dfc42220e54472d21d4e02d2d33
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-24 03:42:02 -0700

    local setup changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const dotenv = require('dotenv'); dotenv.load();
 
 const cors = require('cors')
 
