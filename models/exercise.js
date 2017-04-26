6dcaa7ea25a731beb5752a607ec3a1f5d685a4b0
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-26 16:18:20 -0700

    first version of exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -5,6 +5,26 @@ const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
 
+  username: String,
+  userId : {
+    type: String,
+    required: true,
+    ref: 'User'
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: [30, 'description is too long']
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: [1, 'duration is too short']
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
 
 });
 
